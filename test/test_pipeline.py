a6c7dba6ff7a6b702703668ff80fa8af35d4b496
Phillipe <phillipefs@msn.com>
2023-10-31 21:11:26 -0300

    create unittest

---
+++ test/test_pipeline.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from app.pipeline.transform import concatenate_dataframes
+
+
+df1 = pd.DataFrame({'col1': [1,2], 'col2': [3,4]})
+df2 = pd.DataFrame({'col1': [1,2], 'col2': [3,4]})
+
+def test_concatenate_dfs():
+    """
+    Test function concat dataframes
+    """
+    df = concatenate_dataframes([df1, df2])
+    assert df.shape == (4,2)
+
+
+
