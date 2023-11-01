7ba34f7746615c746e40664defa7382a2ece270e
Phillipe <phillipefs@msn.com>
2023-10-31 21:15:25 -0300

    create script main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from pipeline.extract import extract_from_excel
+from pipeline.transform import concatenate_dataframes
+from pipeline.load import save_dataframe
 
 
 
-
-print(extract_from_excel("data/input"))
+if __name__ == '__main__':
+    dataframes_list = extract_from_excel("data/input")
+    df_full = concatenate_dataframes(dataframes_list)
+    save_dataframe(df_full, "data/output", "consolidate_file")
