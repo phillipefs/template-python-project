6d7d55c188162a64c61b1f12842aa0081518bcf7
Phillipe <phillipefs@msn.com>
2023-11-02 08:50:36 -0300

    update function concatenate dataframes

--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -6,4 +6,9 @@ import pandas as pd
 def concatenate_dataframes(
     list_dataframes: List[pd.DataFrame],
 ) -> pd.DataFrame:
+    """
+    Concatenate list dataframes
+    args:
+        list_dataframes(List[pd.DataFrame]): List Dataframes
+    """
     return pd.concat(list_dataframes, ignore_index=True)
